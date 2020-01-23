7861c7ac3e4b0bfcaa0711a1814843f6068f5d86
Samantha Sengsouvanh <samantha.sengsouvanh@gmail.com>
2020-01-23 11:31:41 -0600

    added eventlistener
    added event listener for motion

--- src/components/Paddle/Paddle.js
+++ src/components/Paddle/Paddle.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Paddle.css";
 
 export default function Paddle() {
-  return <div className="paddle" />;
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    //console.log(e.x, e.y, e.movementX, e.movementY)
+    setPaddleX(e.x);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  console.log("Paddle X =>", paddleX);
+  return (
+    <div
+      className="paddle"
+      style={{
+        left: `${paddleX}px`
+        //transform: 'translateX(calc(-50% + ${paddleX}px))
+      }}
+    />
+  );
 }
